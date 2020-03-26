ec8254f884b2ee6292edb3ba19c07a9755163c03
mnaufalmuafa <naufalmuafa77@gmail.com>
2020-03-27 06:06:57 +0700

    Menambahkan splash

--- app/src/main/java/com/example/adorablepet/splash.java
+++ app/src/main/java/com/example/adorablepet/splash.java
@@ -2,7 +2,9 @@ package com.example.adorablepet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splash extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home=new Intent(splash.this, MainActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },2000);
     }
 }
